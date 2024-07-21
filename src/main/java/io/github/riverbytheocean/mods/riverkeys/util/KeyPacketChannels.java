80d696519e5db16e104c51e292ed8397ead58c6d
riverbytheocean <refractionisaneatconcept@gmail.com>
2024-07-21 12:57:43 -0400

    change channel + bump version

--- src/main/java/io/github/riverbytheocean/mods/riverkeys/util/KeyPacketChannels.java
+++ src/main/java/io/github/riverbytheocean/mods/riverkeys/util/KeyPacketChannels.java
@@ -5,9 +5,9 @@ import net.minecraft.resources.ResourceLocation;
 public class KeyPacketChannels {
 
     // backwards compatibility with AriKeys
-    public static final ResourceLocation HANDSHAKE_CHANNEL = ResourceLocation.fromNamespaceAndPath("arikeys", "greeting");
-    public static final ResourceLocation ADD_KEY_CHANNEL = ResourceLocation.fromNamespaceAndPath("arikeys", "addkey");
-    public static final ResourceLocation LOAD_CHANNEL = ResourceLocation.fromNamespaceAndPath("arikeys", "load");
-    public static final ResourceLocation KEY_CHANNEL = ResourceLocation.fromNamespaceAndPath("arikeys", "keybind");
+    public static final ResourceLocation HANDSHAKE_CHANNEL = ResourceLocation.fromNamespaceAndPath("riverkeys", "greeting");
+    public static final ResourceLocation ADD_KEY_CHANNEL = ResourceLocation.fromNamespaceAndPath("riverkeys", "addkey");
+    public static final ResourceLocation LOAD_CHANNEL = ResourceLocation.fromNamespaceAndPath("riverkeys", "load");
+    public static final ResourceLocation KEY_CHANNEL = ResourceLocation.fromNamespaceAndPath("riverkeys", "keybind");
 
 }
